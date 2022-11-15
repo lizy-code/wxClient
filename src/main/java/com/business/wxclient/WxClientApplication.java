00a63f22414990bbedf64dcf149af2e31dc854a8
lizy <263508896@qq.com>
2022-11-15 11:08:56 +0800

    first commit

---
+++ src/main/java/com/business/wxclient/WxClientApplication.java
@@ -0,0 +1,13 @@
+package com.business.wxclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/business/wxclient/WxClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.wxclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
